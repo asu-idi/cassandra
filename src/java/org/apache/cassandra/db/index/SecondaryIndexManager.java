ff009d8d7e8d30ecbd83fee3bd83a23cfc123206
Dikang Gu <dikang85@gmail.com>
2016-03-28 12:55:54 -0700

    Use noop index updater during compaction when appropriate
    Patch by Dikang Gu; reviewed by Sam Tunnicliffe for CASSANDRA-11450

--- src/java/org/apache/cassandra/db/index/SecondaryIndexManager.java
+++ src/java/org/apache/cassandra/db/index/SecondaryIndexManager.java
@@ -514,7 +514,9 @@ public class SecondaryIndexManager
      */
     public Updater gcUpdaterFor(DecoratedKey key)
     {
-        return new GCUpdater(key);
+        return (indexesByColumn.isEmpty() && rowLevelIndexMap.isEmpty())
+               ? nullUpdater
+               : new GCUpdater(key);
     }
 
     /**
